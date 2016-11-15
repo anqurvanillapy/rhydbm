de219ef467281e86fd62b7403f94a9090c7a1abe
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-11-16 01:06:03 +0800

    init db struct

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ rhydbm
 **Notice:** code for learning, not for production use.
 
 Rhydbm is a C re-implementation of the simple database engine,
-[dbm](https://en.wikipedia.org/wiki/Dbm), which follows an embeded-library
-convention with concise modularity, mainly for coding exercise.
+[dbm](https://en.wikipedia.org/wiki/Dbm), following an concise convention with
+compact modularity, mainly for coding exercise.
 
 License
 -------
--- rhydbm.c
+++ rhydbm.c
@@ -1,8 +1,25 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <stat.h>
+
+typedef struct {
+    /* TODO */
+} hashtable;
+
+typedef struct {
+    mode_t mode;
+    int dir_fd;
+    int dat_fd;
+    int bak_fd;
+    hashtable *index;
+} RhyDBM;
+
+RhyDBM *open(const char *fbname, const char *fmode, mode_t mode);
+static void rhydbm_init(RhyDBM *db);
 
 int
 main(int argc, char const *argv[])
 {
-    /* code */
     return 0;
 }
